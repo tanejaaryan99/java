308f14853e52d2dab90cfa349123449583d91ea4
Aryan Taneja <tanejaaryan85@gmail.com>
2023-01-07 18:25:20 +0530

    nodes at dis k in bt added

--- BinaryTree/BT.java
+++ BinaryTree/BT.java
@@ -11,22 +11,26 @@ public class BT {
         }
     }
     public static void main(String[] args) {
-        Node root = new Node(10);
-        root.left = new Node(20);
-        root.right = new Node(30);
-        root.left.left = new Node(40);
-        root.left.right = new Node(50);
-        root.right.left = new Node(60);
-        postOrder(root);
-        System.out.println();
-        preOrder(root);
-        System.out.println();
-        inOrder(root);
-        System.out.println();
-        System.out.println(height(root));
-        System.out.println(depth(root));
-        System.out.println(heightByPI(root));
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+//        root.right.right = new Node(7);
+//        postOrder(root);
+//        System.out.println();
+//        preOrder(root);
+//        System.out.println();
+//        inOrder(root);
+//        System.out.println();
+//        System.out.println(height(root));
+//        System.out.println(depth(root));
+//        System.out.println(heightByPI(root));
+        nodeAtDisK(root,2);
+//        System.out.println(nodeAtDisK(root,2));
     }
+
 
     public static void postOrder(Node root){
         if(root!=null){
@@ -57,12 +61,24 @@ public class BT {
         int rHeight = height(root.right);
         return Math.max(lHeight,rHeight)+1;
     }
-    public static int heightByPI(Node node)//height starts from 0
+    public static int heightByPI(Node root)//height starts from 0
     {
-        if(node==null)
+        if(root==null)
             return 0;
         else
-            return Math.max(heightByPI(node.left), heightByPI(node.right)+1);
+            return Math.max(heightByPI(root.left), heightByPI(root.right)+1);
+    }
+    public static void nodeAtDisK(Node root , int k){
+        if(k==0 && root != null){
+            System.out.print(root.val + " ");
+        }
+        if(root == null && k == 0) return;
+        if(height(root)==1) return;
+
+        nodeAtDisK(root.left,k-1);
+        nodeAtDisK(root.right,k-1);
+//        System.out.println();
+//        return ansL;
     }
     public static int depth(Node root){
         if(root == null) return -1;

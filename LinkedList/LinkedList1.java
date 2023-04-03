8a9cbbe61d58113126da4fd0e769c31c095bb773
Aryan Taneja <tanejaaryan85@gmail.com>
2023-04-03 10:59:49 +0530

    Update LL

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -1,13 +1,21 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class LinkedList1 {
     public static void main(String[] args) {
         linkedList ll = new linkedList();
-        ll.addFirst(10);
-        ll.addFirst(20);
-        ll.addFirst(30);
-        ll.addFirst(40);
-        ll.addFirst(50);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            int a = sc.nextInt();
+            ll.addLast(a);
+        }
+//        ll.addFirst(10);
+//        ll.addFirst(20);
+//        ll.addFirst(30);
+//        ll.addFirst(40);
+//        ll.addFirst(50);
 //        ll.addFirst(60);
         ll.display();
 //        ll.reverse();
@@ -18,5 +26,7 @@ public class LinkedList1 {
         ll.display();
         ll.foldRecursive();
         ll.display();
+
+
     }
 }
